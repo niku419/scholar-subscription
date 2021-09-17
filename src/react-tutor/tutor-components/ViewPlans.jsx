c8dd5f885119fb7d50b743f73887e7b9361befe6
Niku_419 <2019198@iiitdmj.ac.in>
2021-09-17 21:04:10 +0530

    :hammer: edited view admin plans page

--- src/react-tutor/tutor-components/ViewPlans.jsx
+++ src/react-tutor/tutor-components/ViewPlans.jsx
@@ -3,38 +3,14 @@ import { Container, Card, Row, Col } from 'react-bootstrap'
 import axios from 'axios'
 
 export default function ViewPlans() {
-  const [plans, setplans] = useState({})
+  const [plans, setplans] = useState([])
   useEffect(() => {
     const axiosRequest = async () => {
       const response = await axios.get('http://localhost:3001/get/plans')
-      console.log(...response.data.items)
       setplans(JSON.parse(...response.data.items))
-      console.log(1)
     }
     axiosRequest()
   }, [])
-  async function subscribeThis(plan){
-    console.log(plan)
-    let data = { 
-      plan_id: plan.id, 
-      total_count:12,
-      quantity: 1,
-      customer_notify:1,
-      start_at: Math.floor(Date.now() / 1000), 
-      expire_by:Math.floor((Date.now()) / 1000) + 30*24*60*60, 
-      addons:[ 
-        { 
-          item: { 
-            name: plan.item.name, 
-            amount: plan.item.amount, 
-            currency:"INR" 
-          } 
-        } 
-      ],
-    }
-    const result = await axios.post('http://localhost:3001/create/subscription', data)
-    console.log(result.data)
-  }
 
   return (
     <Container>
@@ -48,7 +24,6 @@ export default function ViewPlans() {
                 <Card.Text>{plan.item.description}</Card.Text>
                 <Card.Text>{plan.item.amount}</Card.Text>
                 <Card.Text>{plan.period}</Card.Text>
-                {/* <Button onClick={() => subscribeThis(plan)}>Create Subscription</Button> */}
               </Card.Body>
             </Card>
           </Col>
