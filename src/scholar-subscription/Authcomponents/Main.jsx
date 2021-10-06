144aeb0137ebbf40857f0a99a1b480fddec90320
Allam Nikhil Sai <2019198@iiitdmj.ac.in>
2021-10-06 14:00:25 +0530

    Update Main.jsx

--- src/scholar-subscription/Authcomponents/Main.jsx
+++ src/scholar-subscription/Authcomponents/Main.jsx
@@ -9,25 +9,24 @@ export default function Main() {
   const { currentUser } = useAuth()
   return (
     <>
-      <Container style={{height: "75vh"}} fluid className="d-flex justify-content-center align-items-center flex-column">
+      <Container style={{height: "75vh"}} fluid className="d-flex justify-content-around align-items-center flex-column">
         {currentUser && (
           role ==="student" && <Redirect to={'/subscriptions'}/>
         )}
         {currentUser && (
           role ==="teacher" && <Redirect to={'/plans'}/>
         )}
-        <NavBar show={false}/>
         <div className="heading">
           <div className="react-heading">
-           Scholar Subscription
+            Scholar Subscription
           </div>
         </div>
-      </Container>
-      <Container style={{height: "10vh"}}>
-        <div className="d-flex justify-content-evenly">
-          <a href="/student-login" role="button" className="btn btn-primary">Student</a>
-          <a href="/teacher-login" role="button" className="btn btn-primary">Teacher</a>
-        </div>
+        <Container style={{height: "10vh"}}>
+          <div className="d-flex justify-content-evenly">
+            <a href="/student-login" role="button" className="btn btn-primary">Student</a>
+            <a href="/teacher-login" role="button" className="btn btn-primary">Teacher</a>
+          </div>
+        </Container>
       </Container>
     </>
   )
