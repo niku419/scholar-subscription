ef75918af8474efc6ca45e10ddc7de6969abf7da
Allam Nikhil Sai <2019198@iiitdmj.ac.in>
2022-02-01 12:52:47 +0530

    Update Main.jsx

--- src/scholar-subscription/Authcomponents/Main.jsx
+++ src/scholar-subscription/Authcomponents/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import NavBar from './NavBar'
 import { Redirect } from 'react-router-dom'
 import { useAuth } from '../Context'
 
@@ -8,24 +9,25 @@ export default function Main() {
   const { currentUser } = useAuth()
   return (
     <>
-      <Container style={{height: "75vh"}} fluid className="d-flex justify-content-around align-items-center flex-column">
+      <NavBar show={false}/>
+      <Container style={{height: "75vh"}} fluid className="d-flex justify-content-center align-items-center">
         {currentUser && (
           role ==="student" && <Redirect to={'/subscriptions'}/>
         )}
         {currentUser && (
           role ==="teacher" && <Redirect to={'/plans'}/>
         )}
-        <div className="heading">
+        <div className="heading breakpoint">
           <div className="react-heading">
-            Scholar Subscription
+           Scholar Subscription
           </div>
         </div>
-        <Container style={{height: "10vh"}}>
-          <div className="d-flex justify-content-evenly">
-            <a href="/student-login" role="button" className="btn btn-primary">Student</a>
-            <a href="/teacher-login" role="button" className="btn btn-primary">Teacher</a>
-          </div>
-        </Container>
+      </Container>
+      <Container style={{height: "10vh"}}>
+        <div className="d-flex justify-content-evenly">
+          <a href="/student-login" role="button" className="btn btn-primary">Student</a>
+          <a href="/teacher-login" role="button" className="btn btn-primary">Teacher</a>
+        </div>
       </Container>
     </>
   )
